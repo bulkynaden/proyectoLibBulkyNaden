cc61bdb9a2015e32fab3fa4a4816dd9ace1577f0
Javier Tomás Acín <63659001+bulkynaden@users.noreply.github.com>
2023-03-16 12:24:46 +0100

    Update PruebaLibreria.java

--- src/main/java/es/mde/acing/utils/PruebaLibreria.java
+++ src/main/java/es/mde/acing/utils/PruebaLibreria.java
@@ -2,7 +2,7 @@ package es.mde.acing.utils;
 
 public class PruebaLibreria {
     public static void imprime() {
-        String mensaje = "Holllllaaa!";
+        String mensaje = "Holllllaaa v1.8!";
        System.out.println(mensaje);
     }
 }
